424e790f3500713c63cc06b433225d77af4bdc88
A7mdos <a7mdos1999@gmail.com>
2022-10-14 20:47:06 +0200

    added signup functionality

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 
+const db = require("./data/database");
+const authRoutes = require("./routes/auth");
+
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(authRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
+  res.send("Hello World!");
+});
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}...`);
-})
+
+const IPV4 = "192.168.43.170";
+const PORT = 8081;
+db.connectToDatabase().then(function () {
+  app.listen(PORT, IPV4, () => {
+    console.log(`Server is running on http://${IPV4}:${PORT} ...`);
+  });
+});
---
+++ back-end/routes/auth.js
@@ -0,0 +1,129 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+
+const db = require("../data/database");
+
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.send("Welcome");
+});
+
+// router.get("/signup", function (req, res) {
+//   let sessionInputData = req.session.inputData;
+
+//   if (!sessionInputData) {
+//     sessionInputData = {
+//       hasError: false,
+//       email: "",
+//       confirmEmail: "",
+//       password: "",
+//     };
+//   }
+
+//   req.session.inputData = null;
+
+//   res.render("signup", { inputData: sessionInputData });
+// });
+
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+
+  const enteredEmail = userData.email;
+  const enteredPassword = userData.password;
+
+  const enteredName = userData.name;
+  const enteredPhone = userData.phone;
+  const enteredAge = userData.age;
+  const enteredGender = userData.gender;
+
+  //   if (
+  //     !enteredEmail ||
+  //     !enteredConfirmEmail ||
+  //     !enteredPassword ||
+  //     enteredPassword.trim().length < 6 ||
+  //     enteredEmail !== enteredConfirmEmail ||
+  //     !enteredEmail.includes("@")
+  //   ) {
+  //     req.session.inputData = {
+  //       hasError: true,
+  //       message: "Invalid input - please check your data.",
+  //       email: enteredEmail,
+  //       confirmEmail: enteredConfirmEmail,
+  //       password: enteredPassword,
+  //     };
+
+  //     req.session.save(function () {
+  //       res.redirect("/signup");
+  //     });
+  //     return;
+  //   }
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (existingUser) {
+    res.json({ msg: "User already exists" });
+    return;
+  }
+
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12);
+
+  const user = {
+    name: enteredName,
+    email: enteredEmail,
+    password: hashedPassword,
+    phone: enteredPhone,
+    gender: enteredGender,
+    age: enteredAge,
+  };
+
+  await db.getDb().collection("users").insertOne(user);
+
+  res.json({ msg: "User created successfully!" });
+  return;
+});
+
+// router.post("/login", async function (req, res) {
+//   const userData = req.body;
+//   const enteredEmail = userData.email;
+//   const enteredPassword = userData.password;
+
+//   const existingUser = await db
+//     .getDb()
+//     .collection("users")
+//     .findOne({ email: enteredEmail });
+
+//   if (!existingUser) {
+//     res.json({ msg: "Couldn't log in - user not registered!" });
+//     return;
+//   }
+
+//   const passwordsAreEqual = await bcrypt.compare(
+//     enteredPassword,
+//     existingUser.password
+//   );
+
+//   if (!passwordsAreEqual) {
+//     res.json({msg: "Couldn't log in - password is incorrect!"});
+//     return res.redirect("/login");
+//   }
+
+//   req.session.user = { id: existingUser._id, email: existingUser.email };
+//   req.session.isAuthenticated = true;
+
+//   req.session.save(function () {
+//     res.redirect("/profile");
+//   });
+// });
+
+// router.post("/logout", function (req, res) {
+//   req.session.user = null;
+//   req.session.isAuthenticated = false;
+
+//   res.redirect("/");
+// });
+
+module.exports = router;
